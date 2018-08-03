c365e743e2f7b192b7c6ceeb1e7ce84335b91d4c
riku.kojima <rikuthekojima@gmail.com>
2018-08-03 01:58:06 +0000

    login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,3 @@
-<!--外部APIの利用法を学ぶ-->
 @extends('layouts.app')
 
 @section('cover')
@@ -6,13 +5,15 @@
         <div class="cover-inner">
             <div class="cover-contents">
                 <h1>素敵なモノと出会う場所</h1>
-                <a href="" class="btn btn-success btn-lg">モノリストを始める</a>
+                <!--ログインがまだなら-->
+                @if (!Auth::check())
+                    <a href="{{ route('signup.get') }}" class="btn btn-success btn-lg">モノリストを始める</a>
+                @endif
             </div>
         </div>
     </div>
 @endsection
 
 @section('content')
-    <!--app.blade.phpのcontentに代入-->
     テスト
 @endsection
