51459d9f51c9908bf6f3e70495d643bb8993dec1
riku.kojima <rikuthekojima@gmail.com>
2018-08-03 01:04:30 +0000

    top page

---
+++ resources/views/commons/error_messages.blade.php
@@ -0,0 +1,6 @@
+<!--errorsが運ばれてくる-->
+@if (count($errors) > 0)
+    @foreach ($errors->all() as $error)
+        <div class="alert alert-warning">{{ $error }}</div>
+    @endforeach
+@endif
---
+++ resources/views/commons/footer.blade.php
@@ -0,0 +1,3 @@
+<footer><footer>
+         <div<div  classclass==""text-center text-mutedtext-center text-muted"">>© 2016 MONOLIST.© 2016 MONOLIST.</div></div>
+ </footer></footer>
